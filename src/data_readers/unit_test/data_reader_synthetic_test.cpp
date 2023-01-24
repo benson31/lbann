5cfe7d449192c7fa215521e3846260202277b3e3
Thomas R. Benson <benson31@llnl.gov>
2023-01-24 17:42:58 -0500

    clang-format

--- src/data_readers/unit_test/data_reader_synthetic_test.cpp
+++ src/data_readers/unit_test/data_reader_synthetic_test.cpp
@@ -220,7 +220,7 @@ TEST_CASE("Synthetic data reader data field",
   std::map<lbann::data_field_type, std::vector<int>> fields;
   int f = 0;
   for (auto const& data_field : data_fields) {
-    std::vector<int> dims = {s+f, s+f};
+    std::vector<int> dims = {s + f, s + f};
     fields[data_field] = dims;
     ++f;
   }
